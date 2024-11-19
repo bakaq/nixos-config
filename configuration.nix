ca13236ee8fc593d31c4f9a325984349feb0b112
bakaq <bakaq@bakaq.net>
2024-11-19 13:00:55 -0300

    Chinese IME

--- configuration.nix
+++ configuration.nix
@@ -305,8 +305,10 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
+    fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
       fcitx5-mozc
+      fcitx5-rime
       fcitx5-gtk
     ];
   };
