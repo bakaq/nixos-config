0086bc7cb641f0525ac6c70058aab47eb87c43ef
bakaq <bakaq@bakaq.net>
2025-01-15 03:36:48 -0300

    Update

--- configuration.nix
+++ configuration.nix
@@ -242,7 +242,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
     prime = {
       offload = {
         enable = true;
